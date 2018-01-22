446bd27bf21705eb677d6221a2f8aa164548e250
khuangaf <khuangaf@connect.ust.hk>
2018-01-22 19:05:22 +0800

    unclip softmax yields better performance

--- policies.py
+++ policies.py
@@ -3,38 +3,54 @@ import tensorflow as tf
 from utils import conv, fc, conv_to_fc, batch_to_seq, seq_to_batch, lstm, lnlstm, conv1d, sample, softmax_sample
 from baselines.common.distributions import make_pdtype
 
+def softmax(w, t=1.0):
+    """softmax that avoid inf/ nan."""
+    eps = 1e-7
+    log_eps = np.log(eps)
+    w = tf.clip_by_value(w, -log_eps, log_eps)  # avoid inf/nan
+    e = tf.exp(w / t)
+    dist = e / tf.reduce_sum(e)
+    return dist
+
+
 class CnnPolicy(object):
-    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=False): #pylint: disable=W0613
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=True): #pylint: disable=W0613
         ob_shape = (nbatch,) + ob_space.shape
         actdim = ac_space.shape[0]
-        print (ob_space)
-        print (ac_space)
+
         
         window_length = ob_space.shape[1] -1
 
         X = tf.placeholder(tf.float32, ob_shape, name='Ob') #obs
-
-        with tf.variable_scope("model", reuse=reuse):
+        
+        
+        with tf.variable_scope("model", reuse=reuse) as scope:
             w0 = tf.slice(X, [0,0,0,0],[-1,-1,1,1])
             x = tf.slice(X, [0,0,1,0],[-1,-1,-1,-1])
-            x = conv(tf.cast(x, tf.float32),'c1', fh=1,fw=3,nf=3, stride=1, init_scale=np.sqrt(2))
-            print (x)
+            
+            # reuse when testing
+            try:
+                x = conv(tf.cast(x, tf.float32),'c1', fh=1,fw=3,nf=3, stride=1, init_scale=np.sqrt(2))
+            except:
+                scope.reuse_variables()
+                x = conv(tf.cast(x, tf.float32),'c1', fh=1,fw=3,nf=3, stride=1, init_scale=np.sqrt(2))
+
             x = conv(x, 'c2', fh=1, fw=window_length -2, nf=20, stride= window_length -2, init_scale=np.sqrt(2))
-            print (x)
+
             x = tf.concat([x, w0], 3)
-            print (x)
+
             x = conv(x, 'c3', fh=1, fw=1, nf=1, stride= 1, init_scale=np.sqrt(2))
-            print (x)
+
             cash_bias = tf.ones([x.shape[0],1,1,1], tf.float32)
             c = tf.concat([cash_bias, x], 1)
             
             v = conv_to_fc(x)
             vf = fc(v, 'v',1)[:,0]
-            print (v)
+       
             f = tf.contrib.layers.flatten(c)
-            print (f)
+       
             pi = tf.nn.softmax(f)
-            print (pi)
+       
             logstd = tf.get_variable(name="logstd", shape=[1, actdim], 
                 initializer=tf.truncated_normal_initializer())
 
@@ -44,8 +60,11 @@ class CnnPolicy(object):
         self.pd = self.pdtype.pdfromflat(pdparam)
 
         a0 = self.pd.sample()
-        a0 = tf.nn.softmax(a0)
-        # print (a0)
+        a0 = tf.nn.softmax(a0) 
+        
+        
+        # a0 = tf.clip_by_value(a0, -eps, eps, 'clip')
+        
         neglogp0 = self.pd.neglogp(a0)
         self.initial_state = None
 
--- train_ppo.py
+++ train_ppo.py
@@ -14,7 +14,7 @@ from baselines.common import set_global_seeds
 from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 from baselines.common.vec_env.vec_frame_stack import VecFrameStack
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
-from baselines.ppo2 import ppo2
+import ppo2
 from policies import CnnPolicy
 import gym
 import logging
@@ -34,7 +34,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 # config.gpu_options.allow_growth = True
 # set_session(tf.Session(config=config))
 
-def train(df, num_timesteps, seed, policy=CnnPolicy):
+def train(df, num_timesteps, seed, policy=CnnPolicy, training=True):
     
     # configure GPU usage
     ncpu = multiprocessing.cpu_count()
@@ -59,30 +59,29 @@ def train(df, num_timesteps, seed, policy=CnnPolicy):
         env = ConcatStates(env)
         env = bench.Monitor(env, logger.get_dir())
         return env
-
+    
 
     env = DummyVecEnv([make_env])
     set_global_seeds(seed)
 
     ppo2.learn(policy=policy, env=env, nsteps=128, nminibatches=4,
-        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
+        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1000,
         ent_coef=.01,
         lr=lambda f : f * 2.5e-4,
         cliprange=lambda f : f * 0.1,
-        total_timesteps=int(num_timesteps * 1.1))
+        total_timesteps=int(num_timesteps * 1.1), training=training)
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(10e8))
+    parser.add_argument('--num-timesteps', type=int, default=int(8e5))
     args = parser.parse_args()
-    logger.configure(dir='./logs/ppo_train')
-    # logger.configure()
+    logger.configure(dir='./logs/ppo_train_unclip')
     df = pd.read_hdf('./data/poloniex_30m.hf',key='train')
-    train(df, num_timesteps=args.num_timesteps, seed=args.seed)
-    logger.configure(dir='./logs/ppo_test')
+    train(df, num_timesteps=int(60000000), seed=args.seed, training=True)
+    logger.configure(dir='./logs/ppo_test_unclip')
     df = pd.read_hdf('./data/poloniex_30m.hf',key='test')
-    train(df, num_timesteps=args.num_timesteps, seed=args.seed)
+    train(df, num_timesteps=48*7, seed=args.seed, training=False)
 
 if __name__ == '__main__':
     main()
